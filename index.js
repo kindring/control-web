455e872e5debb5da1efb9c963efda35911c5c5c8
kindring <2460131004@qq.com>
2020-11-05 16:05:15 +0800

    服务端初步构建

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(route);
 app.listen(serverConfig.port, () => {
     console.log(`服务器成功启动\n
     可以本机访问@http://127.0.0.1:${serverConfig.port}\n
-    局域网访问@http://${tools.ip.getIp()}
+    局域网访问@http://${tools.ip.getIp()}:${serverConfig.port}
     `)
 })
